18d8612e055da5cbe8aae66b87265270cc670777
emilyvm29 <emilylvm2009@gmail.com>
2025-11-30 21:35:20 -0800

    Update madLibsQuiz1-1.py

--- madLibsQuiz1-1.py
+++ madLibsQuiz1-1.py
@@ -8,10 +8,10 @@ print("|      Welcome to MadLibs          | ")
 print("|                                  |")
 print("************************************")
 
-('''MadLibs is a fill-in-the-blanks story
-game. The player must choose words based on the 
-given prompts, and the computer will return a 
-short story that includes the words the user chose.''')
+(''' MadLibs is a fill-in-the-blanks story game. 
+The player must choose words based on the given prompts, 
+and the computer will return a short story that includes
+the words the user chose.''')
 
 play = input("Do you want to play MadLibs?(y/n) ")
 
@@ -22,21 +22,21 @@ if play == "y":
     animal_1 = input("Choose an animal: ")
     adjective_1 = input("Choose an adjective for a feeling: ")
     adjective_2 = input("Choose an adjective: ")
-    adjective_3  input("Choose an adjective: ")
+    adjective_3 = input("Choose an adjective: ")
     animal_2 = input("Choose an animal: ")
     food = input("Choose a food: ")
     
-    print("""
-    Over break I am going camping with " +person_name+". It is important 
-    to be prepared when camping at place , so I made sure to pack a 
-    sleeping bag, flashlight, and a " +noun_1+". The possibility of seeing a 
-    " +animal_1+" makes me feel " +adjective_1+". I am excited to go 
-    hiking on the " +adjective_2+" trail. If I see a " +adjective_3 +" " +animal_2+"
-    on the hike, I will take it home as my new pet! The best part of 
-    camping is eating " +food+" by the campfire!
-    """)
+    print(f"""
+Over break I am going camping with {person_name}. It is important 
+to be prepared when camping at {place}, so I made sure to pack a 
+sleeping bag, flashlight, and a {noun_1}. The possibility of seeing a 
+{animal_1} makes me feel {adjective_1}. I am excited to go 
+hiking on the " {adjective_2} trail. If I see a {adjective_3} {animal_2}
+on the hike, I will take it home as my new pet! The best part of 
+camping is eating {food} by the campfire!
+""")
     
     print("Thanks for playing! Goodbye!")
     
-else play == "n":
+elif play == "n":
     print("Goodbye!")
